3844164821036f25a737d2cbae98ce16efe8d490
Evelio Ornelas <evelioornelas@Evelios-MacBook-Air.local>
2019-12-16 21:06:24 -0700

    HelloWorld

---
+++ HelloWorld.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  TestingSolutions
+//
+//  Created by Evelio Ornelas Jr. on 12/16/19.
+//  Copyright © 2019 Evelio Ornelas Jr. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cout << "Hello World!" << endl;
+    return 0;
+}
