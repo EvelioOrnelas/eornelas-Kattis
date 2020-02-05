b95a7165cdada1866b135f016eb22a9206f3cab8
Evelio Ornelas <evelioornelas@Evelios-MacBook-Air.local>
2020-02-05 11:30:54 -0700

    Solved Carrots

---
+++ Carrots.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  KattisSolving
+//
+//  Created by Evelio Ornelas Jr. on 12/16/19.
+//  Copyright © 2019 Evelio Ornelas Jr. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void start() {
+    int n, p;
+    string desc;
+    cin >> n >> p;
+    for(int i=0; i<n; i++) {
+        cin >> desc;
+    }
+    cout << p << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    start();
+    return 0;
+}
+// Carrots
