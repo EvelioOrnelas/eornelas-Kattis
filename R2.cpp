0fb42eecec406e7b63ab177f79c618fc425ac713
Evelio Ornelas <evelioornelas@Evelios-MacBook-Air.local>
2020-02-06 13:40:02 -0700

    Solved R2

---
+++ R2.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  KattisSolving
+//
+//  Created by Evelio Ornelas Jr. on 12/16/19.
+//  Copyright © 2019 Evelio Ornelas Jr. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void start() {
+    int r1, r2, s;
+    cin >> r1 >> s;
+    r2 = ((r1+s)/2);
+    cout << (2*s-r1) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    start();
+    return 0;
+}
+// R2
