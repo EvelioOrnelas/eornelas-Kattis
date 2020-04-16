6def7b43e7d95046a365e917078cadf480135036
Evelio Ornelas <evelioornelas@Evelios-MacBook-Air.local>
2020-04-15 23:18:11 -0600

    Solved Different

---
+++ Different.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  KattisSolving
+//
+//  Created by Evelio Ornelas Jr. on 12/16/19.
+//  Copyright © 2019 Evelio Ornelas Jr. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void start() {
+    long long x, y;
+    while(cin >> x >> y) {
+        cout << abs(x - y) << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    start();
+    return 0;
+}
+// A Different Problem
+
