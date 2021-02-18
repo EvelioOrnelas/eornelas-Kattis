d11e91cd2fd403e86a49aaca42ade88a327a923f
Evelio Ornelas <evelioornelas@outlook.com>
2021-02-18 13:05:43 -0700

    Solved jackolanternjuxtaposition

---
+++ jackolanternjuxtaposition.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  KattisSolving
+//
+//  Created by Evelio Ornelas Jr. on 12/16/19.
+//  Copyright © 2019 Evelio Ornelas Jr. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void start() {
+    int N, T, M;
+    cin >> N >> T >> M;
+    cout << N*T*M << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    start();
+    return 0;
+}
+// Jack-O'-Lantern Juxtaposition
+
