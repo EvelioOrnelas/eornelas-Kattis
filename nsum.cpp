028e29a23aad9a8c0e0e65c4262c9b9b92314b18
BloodHound022 <44846885+BloodHound022@users.noreply.github.com>
2021-10-30 20:57:25 -0600

    Solved nsum

---
+++ nsum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void start() {
+    int n, z, sum = 0;
+    vector <int> numbers;
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        cin >> z;
+        numbers.push_back(z);
+    }
+    for(auto i : numbers) {
+        sum += i;
+    }
+    cout << sum << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    start();
+    return 0;
+}
