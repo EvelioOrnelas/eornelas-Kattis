9dc99c9ce432eda6cf2fed40d22d5c2114880150
BloodHound022 <44846885+BloodHound022@users.noreply.github.com>
2021-10-31 21:04:57 -0600

    Solved addtwonumbers

---
+++ addtwonumbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void start() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    start();
+    return 0;
+}
